dbd5484743bc23398ecf05042226ad7f64fc65df
Thomas <thomascaycedo19@outlook.com>
2018-12-23 17:00:18 -0500

    Se validan los campos

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -5,13 +5,18 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
+  styles:[`
+      .ng-invalid.ng-touched:not(form){
+        border: 1px solid red
+      }
+  `]
 })
 export class TemplateComponent  {
 
   usuario:Object ={
-    nombre:"Thomas",
-    apellido:"Caycedo",
-    correo:""
+    nombre:null,
+    apellido:null,
+    correo:null
   }
 
   constructor() { }
